fb92e3baea7045e4667a25be639208eac6801c80
snsie <scottnsiegel@gmail.com>
2022-12-20 15:07:33 -0500

    initialize server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+// server.listen({ port: 9000 });
+const { url } = await server.listen({ port: 9000 });
+console.log(`Server running at ${url}`);
